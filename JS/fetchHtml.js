29f2581a38792e5b9063e0ca038ce45d9c9a32e4
Mani111355 <mani111355@gmail.com>
2024-12-27 20:25:00 +0530

    Update fetchHtml.js

--- JS/fetchHtml.js
+++ JS/fetchHtml.js
@@ -4,3 +4,10 @@ fetch('https://mani111355.github.io/A1-Report/HTML/header.html')
   document.getElementById('header-content').innerHTML = data;
 })
 .catch(error => console.error('Error fetching the HTML:', error));
+
+fetch('https://mani111355.github.io/A1-Report/HTML/container.html')
+.then(response => response.text())
+.then(data => {
+  document.getElementById('container-content').innerHTML = data;
+})
+.catch(error => console.error('Error fetching the HTML:', error));
